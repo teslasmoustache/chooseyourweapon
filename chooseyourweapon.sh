cb6d83bc75da90ace8a7b6f6956243025ba16c3f
teslasmoustache <teslas_moustache@riseup.net>
2014-01-19 01:21:09 -0600

    Update chooseyourweapon.sh
    added some coin ascii art and fixed the coin-toss function.

--- chooseyourweapon.sh
+++ chooseyourweapon.sh
@@ -31,9 +31,7 @@ echo "Choose your weapon."
 #================
 
 function D-4 {
-#generates a random number between 1 and 4.
 NUMBER=$[ ( $RANDOM % 4 ) + 1 ]
-#print out an ascii drawing of a 4-sided die
      echo "               ^"
      echo "              /|\ "
      echo "             /'|:\ "
@@ -53,10 +51,8 @@ NUMBER=$[ ( $RANDOM % 4 ) + 1 ]
 }
 
 function D-6 {
-#generates a random number between 1 and 6.
 NUMBER=$[ ( $RANDOM % 6 ) + 1 ]
-#prints out an ascii drawing of a 6-sided die
-echo " _______     "
+echo " _______.    "
 echo "| .   . |\   "
 echo "|   .   |.\  "
 echo "| .   . |.'| "
@@ -68,43 +64,65 @@ echo $NUMBER
 }
 
 function D-8 {
-#generates a random number between 1 and 8.
 NUMBER=$[ ( $RANDOM % 8 ) + 1 ]
 echo " You have chosen D8. And you have rolled a.... "
 echo $NUMBER
 }
 
 function D-10 {
-#generates a random number between 1 and 10.
 NUMBER=$[ ( $RANDOM % 10 ) + 1 ]
 echo " You have chosen D10. And you have rolled a.... "
 echo $NUMBER
 }
 
 function D-12 {
-#generates a random number between 1 and 12.
 NUMBER=$[ ( $RANDOM % 12 ) + 1 ]
 echo " You have chosen D12. And you have rolled a.... "
 echo $NUMBER
 }
 
 function D-20 {
-#generates a random number between 1 and 20.
 NUMBER=$[ ( $RANDOM % 20 ) + 1 ]
 echo " You have chosen D20. And you have rolled a.... "
 echo $NUMBER
 }
 
 function D-100 {
-#generates a random number between 1 and 100.
 NUMBER=$[ ( $RANDOM % 100 )  + 1 ]
 echo " You have chosen D100. And you have rolled a.... "
 echo $NUMBER
 }
 
 function coin-toss {
-#generates a random number between 1 and 2, and returns "1" as "heads" and "2" as "tails".
 NUMBER=$[ ( $RANDOM % 2 ) + 1 ]
+echo " Feel like donating? 14ZcmYJCZKsUFDYfHdU7YEaAwkatLdBvVC             "
+echo ""
+echo "                  ,.=ctE55ttt553tzs.,                               "
+echo "              ,,c5;z==!!::::  .::7:==it3>.,                         "
+echo "           ,xC;z!::::::    ::::::::::::!=c33x,                      "
+echo "         ,czz!:::::  ::;;..===:..:::   ::::!ct3.                    "
+echo "       ,C;/.:: :  ;=c!:::::::::::::::..      !tt3.                  "
+echo "      /z/.:   :;z!:::::J  :E3.  E:::::::..     !ct3.                "
+echo "    ,E;F   ::;t::::::::J  :E3.  E::.     ::.     \ttL               "
+echo "   ;E7.    :c::::F******   **.  *==c;..    ::     Jttk              "
+echo "  .EJ.    ;::::::L                   '|:.   ::.    Jttl             "
+echo "  [:.    :::::::::773.    JE773zs.     I:. ::::.    It3L            "
+echo " ;:[     L:::::::::::L    |t::!::J     |::::::::    :Et3            "
+echo " [:L    !::::::::::::L    |t::;z2F    .Et:::.:::.  ::[13            "
+echo " E:.    !::::::::::::L               =Et::::::::!  ::|13            "
+echo " E:.    (::::::::::::L    .......       \:::::::!  ::|i3            "
+echo " [:L    !::::      ::L    |3t::::!3.     ]::::::.  ::[13            "
+echo " !:(     .:::::    ::L    |t::::::3L     |:::::; ::::EE3            "
+echo "  E3.    :::::::::;z5.    Jz;;;z=F.     :E:::::.::::II3[            "
+echo "  Jt1.    :::::::[                    ;z5::::;.::::;3t3             "
+echo "   \z1.::::::::::l......   ..   ;.=ct5::::::/.::::;Et3L             "
+echo "    \t3.:::::::::::::::J  :E3.  Et::::::::;!:::::;5E3L              "
+echo "     'cz\.:::::::::::::J   E3.  E:::::::z!     ;Zz37'               "
+echo "       \z3.       ::;:::::::::::::::;=       ./355F                 "
+echo "         \z3x.         ::~=======          ,c253F                   "
+echo "           'tz3=.                      ..c5t32^                     "
+echo "              '=zz3==...         ...=t3z13P^                        "
+echo "                  '*=zjzczIIII3zzztE3>*^'                           "
 echo " You have tossed a coin. And it has landed on.... "
 if [ $NUMBER -eq 1 ]
 then
